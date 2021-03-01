992e15f4484466116085f23e9a5e559e50be6101
9SuJin8 <sstt9884@gmail.com>
2021-03-01 16:48:32 +0900

    Click and show toast message

---
+++ kotlin/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(applicationContext, "Test", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+}
